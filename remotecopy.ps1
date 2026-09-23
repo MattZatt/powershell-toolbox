f15c367bafd4687db66c581af92f5ff3c388ff70
MattZatt <103581131+MattZatt@users.noreply.github.com>
2026-09-23 11:39:35 -0500

    Add files via upload
    Prototype for Data migration  via powershell
    assisted development from vscode suggestions with rewrite as needed

---
+++ remotecopy.ps1
@@ -0,0 +1,45 @@
+function Copy-RemoteFile {
+    param (
+        [string]$NetworkDrive,
+        [string]$Username,
+        [securestring]$Password,
+        [string]$SourcePath,
+        [string]$DestinationPath
+    )
+
+    # Find Available Drive D..Z in reverse order
+    $drv = 90..69 | ForEach-Object{[char]$_}
+    foreach ($drv in $drv) {
+
+    if (Get-PSDrive $drv -ErrorAction SilentlyContinue) {
+        Write-Host "The ${drv}: drive is already in use."
+    } else {
+        Write-Host "The ${drv}: drive is available."
+        break
+    }
+    }
+    $NetworkDrive = "${drv}:"
+
+    # Map the network drive
+    net use $NetworkDrive /user:$Username $Password
+
+    # Change to the source directory
+    Set-Location $SourcePath
+    try {
+        test-path $DestinationPath -ErrorAction Stop
+    }
+    catch {
+        Write-Error "Destination path does not exist or is not accessible."
+        return
+    }
+    # Copy the files to the destination
+    copy-item $SourcePath -destination $DestinationPath -Recurse
+
+    # Disconnect the network drive
+    net use $NetworkDrive /delete
+}
+#net use <network drive> location /user:<username> <password>
+#cd <file path>
+#copy-item <file path> -destination #destination path -Recurse if full path
+#when done net use #network drive location /delete
+Copy-RemoteFile
